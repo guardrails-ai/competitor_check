f1f72a12b9e8b5a922cabecb29953d8aa21981ce
Caleb Courier <caleb.courier@gmail.com>
2024-02-16 13:08:27 -0600

    download models during post-install

--- validator/post-install.py
+++ validator/post-install.py
@@ -1,3 +1,16 @@
-print("post-install starting...")
-print("This is where you would do things like download nltk tokenizers or login to the HuggingFace hub...")
-print("post-install complete!")
+import nltk
+import spacy
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+
+model = "en_core_web_trf"
+if not spacy.util.is_package(model):
+    print(
+        f"Spacy model {model} not installed. "
+        "Download should start now and take a few minutes."
+    )
+    spacy.cli.download(model)  # type: ignore
