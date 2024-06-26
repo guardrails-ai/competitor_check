eb06894c3524f1f99d19db16b6a84d6e9f72ca37
Nicholas Chen <nichwch@gmail.com>
2024-06-26 13:13:13 -0400

    formatting

--- validator/main.py
+++ validator/main.py
@@ -168,7 +168,8 @@ class CompetitorCheck(Validator):
             start = 0
             while True:
                 start = a_str.find(sub, start)
-                if start == -1: return
+                if start == -1: 
+                    return
                 yield start
                 start += len(sub) # use start += 1 to find overlapping matches
 
