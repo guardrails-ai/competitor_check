a56facf6cbf391eaa1df3e8c2fa89200578c3896
Wyatt Lansford <22553069+wylansford@users.noreply.github.com>
2024-07-01 15:27:36 -0700

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,7 +1,7 @@
 INPUT_SCHEMA = {
     "text": {
         "example": [
-            "I hate you, I will smack you, this is a toxic first sentence. But this is a friendly sentence."
+           "Apple made the iphone"
         ],
         "shape": [1],
         "datatype": "STRING",
