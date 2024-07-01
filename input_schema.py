f1ba6eee2363d3fca1ff944a1c62705ca4a876bf
Wyatt Lansford <22553069+wylansford@users.noreply.github.com>
2024-07-01 15:07:09 -0700

    inferless added

---
+++ input_schema.py
@@ -0,0 +1,16 @@
+INPUT_SCHEMA = {
+    "text": {
+        "example": [
+            "I hate you, I will smack you, this is a toxic first sentence. But this is a friendly sentence."
+        ],
+        "shape": [1],
+        "datatype": "STRING",
+        "required": True,
+    },
+    "competitors": {
+        "example": ["Apple"],
+        "shape": [-1],
+        "datatype": "STRING",
+        "required": True,
+    },
+}
