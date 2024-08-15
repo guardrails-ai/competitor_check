34075d741bade80601d13fa1468053af83d964bf
zsimjee <zaydsimjee@gmail.com>
2024-08-15 15:11:11 -0700

    fastify app

--- app.py
+++ app.py
@@ -1,26 +1,74 @@
-import nltk
-from typing import Any, Dict
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List
 import spacy
-class InferlessPythonModel:
+import os
 
-    def initialize(self):
-        spacy.require_gpu()
-        spacy.cli.download("en_core_web_trf")
-        self.nlp = spacy.load("en_core_web_trf")
-        nltk.download('punkt')
-        
-    def infer(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
-        text = inputs["text"]
-        competitors = inputs["competitors"]
+app = FastAPI()
+# Initialize the detoxify model once
+env = os.environ.get("env", "dev")
+torch_device = "cuda" if env == "prod" else "cpu"
 
-        doc = self.nlp(text)
-        anonymized_text = text
-        for ent in doc.ents:
-            if ent.text in competitors:
-                anonymized_text = anonymized_text.replace(ent.text, "[COMPETITOR]")
-        return {"result": anonymized_text}
-        
-    def finalize(self):
-        pass
+
+class InferenceData(BaseModel):
+    name: str
+    shape: List[int]
+    data: List
+    datatype: str
+
+
+class InputRequest(BaseModel):
+    inputs: List[InferenceData]
+
+
+class OutputResponse(BaseModel):
+    modelname: str
+    modelversion: str
+    outputs: List[InferenceData]
+
+class CompetitorCheck:
+    model_name = "en_core_web_trf"
+    nlp = spacy.load(model_name)
+
+    def infer(text_vals, competitors):
+        outputs = []
+        for idx, text in enumerate(text_vals):
+            doc = CompetitorCheck.nlp(text)
+
+            located_competitors = []
+            for ent in doc.ents:
+                if ent.text in competitors:
+                    located_competitors.append(ent.text)
+
+            outputs.append(
+                InferenceData(
+                    name=f"result{idx}",
+                    datatype="BYTES",
+                    shape=[1],
+                    data=[located_competitors],
+                )
+            )
+
+        output_data = OutputResponse(
+            modelname=CompetitorCheck.model_name, modelversion="1", outputs=outputs
+        )
+
+        return output_data.model_dump()
     
-    
+
+
+
+
+@app.post("/validate", response_model=OutputResponse)
+async def competitor_check(input_request: InputRequest):
+    competitors = []
+    for inp in input_request.inputs:
+        if inp.name == "text":
+            text_vals = inp.data
+        elif inp.name == "competitors":
+            competitors = inp.data
+
+    if text_vals is None or competitors is None:
+        raise HTTPException(status_code=400, detail="Invalid input format")
+
+    return CompetitorCheck.infer(text_vals, competitors)
